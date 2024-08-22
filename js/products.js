c252e846c7ff6a353cbba812b1ed14f6f124924e
Sofia <sofleitas09@gmail.com>
2024-08-21 23:54:33 -0300

    Categoría Autos

---
+++ js/products.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function(){
+    getJSONData(PRODUCTS_URL).then(function(resultado){
+        if(resultado.status = 'ok'){
+            console.log(resultado.data);
+            mostrarAutos(resultado.data.products);
+        }
+    })
+})
+
+function mostrarAutos(array){
+    let mostrar = document.getElementById("autitos");
+    array.forEach((element) => {
+        mostrar.innerHTML += `
+        <div class="row row-cols-1 row-cols-md-3">
+            <div class="col">
+                <div class="card h-100">
+                <img src="${element.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${element.name}</h5>
+                    <p class="card-text">${element.description}</p>
+                </div>
+                </div>
+            </div>
+        </div>
+        `;
+    });
+}
