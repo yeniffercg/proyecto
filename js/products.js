3af4eb6e2fea2df0b400e468db0df98a1ef97b2b
Sofia <sofleitas09@gmail.com>
2024-08-23 17:44:34 -0300

    actualizo el estilo de  los productos

--- js/products.js
+++ js/products.js
@@ -8,17 +8,17 @@ document.addEventListener("DOMContentLoaded", function(){
 })
 
 function mostrarAutos(array){
-    let mostrar = document.getElementById("autitos");
+    let mostrar = document.getElementById("productos");
     array.forEach((element) => {
         mostrar.innerHTML += `
             <div class="col">
-                <div class="card h-100">
-                <img src="${element.image}" class="card-img-top" alt="...">
+                <div class="card h-100 producto">
+                <img src="${element.image}" class="card-img-top" alt="${element.name}">
                 <div class="card-body">
-                    <h5 class="card-title">${element.name}</h5>
+                    <h5 class="card-title nombreProd">${element.name}</h5>
                     <p class="card-text">${element.description}</p>
                     <h5 class="card-text">${element.currency} ${element.cost}</h5>
-                    <p class="card-text">${element.soldCount} productos vendidos</p>
+                    <p class="card-text vendidos">${element.soldCount} productos vendidos</p>
                 </div>
                 </div>
             </div>
