e19cd1ef935629c23e4133c2c2680865f4ee3c1a
Emilia-laborde <emilia.laborde98@gmail.com>
2024-10-15 18:20:09 -0300

    Validación campos obligatorios

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,3 +1,17 @@
 if (user) {
     document.getElementById('userProfile').setAttribute("value", user)
   }
+
+document.getElementById('botonGuardar').addEventListener('click', function(){
+
+ const name = document.getElementById('name1');
+ const lastName = document.getElementById('lastName1');
+
+  if(!name || !lastName){
+    alert('Debe llenar los campos obligatorios para continuar!');
+  }
+ const data = {name, lastName}
+ localStorage.setItem('userData', JSON.stringify(data))
+    alert('datos guardados con exitooo')
+
+})
