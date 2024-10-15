6fa8fa43e2097fd97ab2727f1339e36d378506c3
yeniffercg <coccoyeniffer@gmail.com>
2024-10-15 15:58:59 -0300

    Avance pauta 4

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,3 +1,18 @@
 if (user) {
     document.getElementById('userProfile').setAttribute("value", user)
   }
+
+
+const darkMode = document.getElementById('flexSwitchCheckDefault');
+const isNightMode = localStorage.getItem('nightMode');
+
+    if (isNightMode === 'true') {
+        document.body.classList.add('night-mode');
+        darkMode.checked = true;
+    }
+
+    darkMode.addEventListener('click', () => {
+        const nightModeActivated = darkMode.checked;
+        document.body.classList.toggle('night-mode', nightModeActivated);
+        localStorage.setItem('nightMode', nightModeActivated);
+});
