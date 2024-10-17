ed3a40b21110b2aba741bb1ab5fcdf0e7b517325
moniquemariadarosa <moniqueeei53@gmail.com>
2024-10-17 15:07:21 -0300

    Desafio 5
    cambiar foto perfil

--- js/my-profile.js
+++ js/my-profile.js
@@ -15,3 +15,27 @@ document.getElementById('botonGuardar').addEventListener('click', function(){
     alert('datos guardados con exitooo')
 
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+  const imageInput = document.getElementById("imageUpload");
+  const profileImage = document.getElementById("profileImage");
+
+  const imagenGuardada = localStorage.getItem("profileImage");
+  if (imagenGuardada) {
+    profileImage.src = imagenGuardada;
+}
+
+imageInput.addEventListener("change", function (event) {
+    const lector = new FileReader();
+    lector.onload = function () {
+        profileImage.src = lector.result;
+        localStorage.setItem("profileImage", lector.result);
+    };
+    lector.readAsDataURL(event.target.files[0]);
+  });
+
+const usuarioAlmacenado = localStorage.getItem("username");
+ if (usuarioAlmacenado) {
+    document.getElementById("userProfile").setAttribute("value", usuarioAlmacenado);
+ }
+});
