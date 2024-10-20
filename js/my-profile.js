bdb5f456c9860079b02c958302279c6f682c1759
Sofia <sofleitas09@gmail.com>
2024-10-20 18:02:30 -0300

    Pauta 3

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,37 +1,23 @@
-/*  Codigo de Emilia
-if (user) {
-    document.getElementById('userProfile').setAttribute("value", user)
-  }
-
-document.getElementById('botonGuardar').addEventListener('click', function(){
-
- const name = document.getElementById('name1').value;
- const lastName = document.getElementById('lastName1').value;
- const email = document.getElementById('email').value;
-
-  if(!name || !lastName || !email){
-    alert('Debe llenar los campos obligatorios para continuar!');
-  } else {
-    alert('datos guardados con exitooo');
-  }
-  const data = {name, lastName}
-  localStorage.setItem('userData', JSON.stringify(data))
-})*/
-
 if (user) {
   document.getElementById('userProfile').setAttribute("value", user);
-  document.getElementById('email').value = user.email;
   document.getElementById('arrobaName').textContent = user;
-}
+} else {}
+
+const userData = JSON.parse(localStorage.getItem('userData'));
+if (userData) {
+  document.getElementById('arrobaName').textContent = userData.name + userData.lastName;
+  document.getElementById('arroba').textContent = userData.email;
+} else {}
 
 document.getElementById('botonGuardar').addEventListener('click', function() {
   const name = document.getElementById('name1');
   const lastName = document.getElementById('lastName1');
   const email = document.getElementById('email');
+  const userProfile = document.getElementById('userProfile');
 
   let isValid = true;
 
-  [name, lastName, email].forEach(field => {
+  [name, lastName, email, userProfile].forEach(field => {
     if (!field.value) {
       field.classList.remove('valid');
       field.classList.add('invalid');
@@ -42,7 +28,10 @@ document.getElementById('botonGuardar').addEventListener('click', function() {
     }
   });
 
-  
+  if(userProfile.value && userProfile.value !== user) {
+    localStorage.setItem("user", userProfile.value);
+  }
+
   if (email.value && !email.value.includes('@')) {
     email.classList.remove('valid');
     email.classList.add('invalid');
@@ -52,11 +41,11 @@ document.getElementById('botonGuardar').addEventListener('click', function() {
   if (isValid) {
     const data = { name: name.value, lastName: lastName.value, email: email.value };
     localStorage.setItem('userData', JSON.stringify(data));
-    console.log(localStorage.getItem('userData'))
+    console.log(localStorage.getItem('userData'));
+    document.getElementById('arrobaName').textContent = `${name.value} ${lastName.value}`;
+    document.getElementById('arroba').textContent = email.value;
+    document.getElementById('userTop').textContent = userProfile.value;
   }
-
-  document.getElementById('arrobaName').textContent = `${name.value} ${lastName.value}`;
-  document.getElementById('arroba').textContent = email.value;
 });
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -86,19 +75,22 @@ const usuarioAlmacenado = localStorage.getItem("username");
 });
 
 const theme = document.getElementById('tema');
+const dwMenu = document.getElementById('dwMenu');
 const darkMode = document.getElementById('flexSwitchCheckDefault');
 const isNightMode = localStorage.getItem('nightMode');
 
-    if (isNightMode === 'true') {
-        theme.classList.add('night-mode');
-        document.body.classList.add('night-mode');
-        darkMode.checked = true;
-    }
+if (isNightMode === 'true') {
+  dwMenu.classList.add('night-mode');
+  theme.classList.add('night-mode');
+  document.body.classList.add('night-mode');
+  darkMode.checked = true;
+}
 
-    darkMode.addEventListener('click', () => {
-        const nightModeActivated = darkMode.checked;
-        theme.classList.toggle('night-mode', nightModeActivated);
-        document.body.classList.toggle('night-mode', nightModeActivated);
-        localStorage.setItem('nightMode', nightModeActivated);
+darkMode.addEventListener('click', () => {
+  const nightModeActivated = darkMode.checked;
+  dwMenu.classList.toggle('night-mode', nightModeActivated);
+  theme.classList.toggle('night-mode', nightModeActivated);
+  document.body.classList.toggle('night-mode', nightModeActivated);
+  localStorage.setItem('nightMode', nightModeActivated);
 });
 
