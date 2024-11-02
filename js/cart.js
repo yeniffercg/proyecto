911fd7e7e7e77e23f00ca3986b24ce000a372b6f
yeniffercg <coccoyeniffer@gmail.com>
2024-11-02 19:07:25 -0300

    arreglos cantidades

--- js/cart.js
+++ js/cart.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
         resume.style.display = "none";
         list.innerHTML += `
             <div class="container">
-                <div class="alert alert-danger text-center" role="alert">
+                <div class="alert text-center" role="alert">
                     <h4 class="alert-heading">Aún no tienes productos en el carrito.</h4>
                 </div>
                 <div class="d-md-flex justify-content-md-center">
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function() {
         console.error ("No hay elementos en el local storage!")
     } else {
         console.log(cart);
-        cart.forEach((product) => {
+        cart.forEach((product, index) => {
             list.innerHTML += `
                 <div class="list-group-item list-group-item-action cursor-active">
                     <div class="row">
@@ -33,33 +33,31 @@ document.addEventListener("DOMContentLoaded", function() {
                             </div>
                             <div class="d-inline">
                                 <div id="cant" class="d-inline">
-                                    <button id="restar">-</button>
-                                    <label for="quantity"></label>
-                                    <input type="number" id="cantidad" name="quantity" value="1">
-                                    <button id="sumar">+</button>
+                                    <button onclick="actualizarCantidad(${index}, -1)">-</button>                                    
+                                    <input type="number" id="input-cantidad-${index}" min="1" max="100" name="quantity"
+                                    value="${product.cantidad}" readonly>
+                                    <button onclick="actualizarCantidad(${index}, 1)">+</button>
                                 </div>
-                                <div class="d-inline">
-                                    <svg onclick="removeItem(${product.id})" xmlns="http://www.w3.org/2000/svg" width="25" 
-                                    height="30" fill="currentColor" 
-                                    class="bi bi-trash3" viewBox="0 0 16 16">
-                                        <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0
-                                        0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 
-                                        4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846
-                                        10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1
-                                        .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1
-                                            .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1
-                                            .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
-                                    </svg>
-                                </div>
+                                <svg onclick="removeItem(${product.id})" xmlns="http://www.w3.org/2000/svg" width="25" 
+                                height="30" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                                    <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0
+                                    0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 
+                                    4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846
+                                    10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1
+                                    .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1
+                                    .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1
+                                    .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+                                </svg>
                             </div>
                         </div>
                     </div>
                 </div>`;
             resumeList.innerHTML += `
-                <h5 class="fw-normal my-3">${product.name}<span class="float-end">${product.currency} ${product.cost}</span></h5>`;
+                <h5 class="fw-normal my-3">${product.name}<span class="float-end" id="subtotal-${index}">
+                 ${product.currency} ${(product.cost * product.cantidad)}</span></h5>`;
         });
         calcularSubtotal(cart);
-        actualizarSubtotal(cart);
+        // actualizarSubtotal(cart);
     }
 });
 
@@ -77,34 +75,31 @@ function removeItem(id){
     location.reload();
 }
 
-//Actualiza precio segun cantidad
-document.addEventListener("DOMContentLoaded", function(){
-    valorInicial = document.getElementById("cantidad");
-    btnSumar = document.getElementById("sumar")
-    btnRestar = document.getElementById("restar")
-    cantidad = Number(valorInicial.value);
+// //Actualiza precio segun cantidad
+// document.addEventListener("DOMContentLoaded", function(){
+//     valorInicial = document.getElementById("cantidad");
+//     btnSumar = document.getElementById("sumar")
+//     btnRestar = document.getElementById("restar")
+//     // cantidad = Number(valorInicial.value);
 
-    actualizarSubtotal = () => {
-        calcularSubtotal(cart)
-    }
+//     actualizarSubtotal = () => {
+//         calcularSubtotal(cart)
+//     }
 
-    btnSumar.addEventListener('click', function (){
-        cantidad += 1;
-        valorInicial.value = cantidad;
-        actualizarSubtotal();
+//     btnSumar.addEventListener('click', function (){
+//         cantidad ++;
+//         valorInicial.value = cantidad;
+//         actualizarSubtotal();
 
-});
-    btnRestar.addEventListener('click', function (){
-        if (cantidad > 0) {
-            cantidad -= 1;
-            valorInicial.value = cantidad;
-            actualizarSubtotal();
-        }
-   
-   })
-
- 
- });
+//     });
+//     btnRestar.addEventListener('click', function (){
+//         if (cantidad > 0) {
+//             cantidad --;
+//             valorInicial.value = cantidad;
+//             actualizarSubtotal();
+//         }
+//    })
+// }); https://meet27.webex.com/meet/pr27996126197
 
 
 
@@ -113,14 +108,13 @@ function calcularSubtotal(cart){
     subtotalUSD = document.getElementById("subtotalUSD");
     USD = (product) => (product).currency == "USD";
     UYU = (product) => (product).currency == "UYU";
-    cantidadInput = document.getElementById("cantidad");
-    cantidad = Number(cantidadInput.value) || 0;
+    // cantidad = product.cantidad;
     total = 0;
     
     if(cart.every(USD) || cart.every(UYU)){
         cart.forEach((product) => {
           
-            total += product.cost * cantidad;
+            total += product.cost; //* cantidad;
             subtotal.innerHTML = `${product.currency}` + ' ' + total;
         });
     }else{
@@ -129,31 +123,79 @@ function calcularSubtotal(cart){
         totalUSD = 0;
         totalUYU = 0;
         productsUSD.forEach((product) => {
-            totalUYU += product.cost * 41 * product.cantidad;
-            totalUSD += product.cost * product.cantidad;
+            totalUYU += product.cost * 41; //* product;
+            totalUSD += product.cost //* product.cantidad;
         });
         productsUYU.forEach((product) => {
-            totalUYU += product.cost * product.cantidad;
-            totalUSD += Math.floor(product.cost / 41) * product.cantidad;
+            totalUYU += product.cost; //* product.cantidad;
+            totalUSD += Math.floor(product.cost / 41) //* product.cantidad;
         });
         subtotal.innerHTML = 'UYU ' + totalUSD;
         subtotalUSD.innerHTML = 'USD ' + totalUSD;
 
-        actualizarSubtotal(cart);
+        //actualizarSubtotal(cart);
     } 
 }
 
+// guardar nueva cantidad en el local
+// // boton vaciar carrito
 
+//    function actualizarSubtotal(cart){
+//       const cantidades = document.getElementById("cantidad");
+//       cantidades.addEventListener("input", function(e) {
+//          cantidad = e.target.value;
+//          cart => (product).cantidad == cantidad;
+//      });
+//  }
 
+ function actualizarCantidad(index, cambio) {
+    const cart = JSON.parse(localStorage.getItem('cart'));
+    cart[index].cantidad = Math.max(1, cart[index].cantidad + cambio);
 
+    const cantidadInput = document.getElementById(`input-cantidad-${index}`);
+    cantidadInput.value = cart[index].cantidad;
 
-   function actualizarSubtotal(cart){
-      const cantidades = document.getElementById("cantidad");
-      cantidades.addEventListener("input", function(e) {
-         cantidad = e.target.value;
-         cart => (product).cantidad == cantidad;
-     });
- }
+    const nuevoSubtotal = (cart[index].currency + ' ' + cart[index].cost * cart[index].cantidad);
+    document.getElementById(`subtotal-${index}`).textContent = nuevoSubtotal;
+   
+    localStorage.setItem('cart', JSON.stringify(cart));
+
+    calcularSubtotal(cart);
+    // actualizarResumen(cart);
+}
+
+// function actualizarTotales() {
+
+//     let unidades = 0;
+//     let precio = 0;
+
+//     if (products.length > 0) {
+//         products.forEach(producto => {
+//             unidades += producto.quantity;
+//             precio += producto.cost * producto.quantity;
+//         });
+//     }
+
+//     const unidadesElement = document.getElementById("unidades");
+//     const precioElement = document.getElementById("precio");
+//     const cartCountElement = document.getElementById("cart-count");
+
+//     if (unidadesElement && precioElement) {
+//         unidadesElement.innerText = unidades;
+//         precioElement.innerText = precio;
+//     }
+
+//     // Actualiza el badge del carrito
+//     if (cartCountElement) {
+//         cartCountElement.innerText = unidades;
+//         if (unidades === 0) {
+//             cartCountElement.style.display = "none";
+//          } else { 
+//             cartCountElement.style.display = "inline-block";
+//          }
+//     }
+// }
+
 
 document.getElementById("contCompra").addEventListener("click", function() {
     window.location.href = "categories.html";
