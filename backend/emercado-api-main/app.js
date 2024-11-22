ab6f0f49de6a37775ada228b9bb8572bce08b9fd
yeniffercg <coccoyeniffer@gmail.com>
2024-11-22 20:31:34 -0300

    Co-authored-by: Sofía <sofiadfc@users.noreply.github.com> Co-authored-by: limamoniquee@outlook.com <limamoniquee@outlook.com> Co-authored-by: Emilia-laborde <Emilia-laborde@users.noreply.github.com>

--- backend/emercado-api-main/app.js
+++ backend/emercado-api-main/app.js
@@ -2,23 +2,18 @@ const express = require('express');
 const app = express(); 
 const port = 3000; 
 
-// app.use(express.json());
+app.use(express.json());
 
-// const cart = require('./cart/buy.json'); 
-// app.get( '/cart' , (req, res) => {
-//     res.send (res.json);
-// }); 
-// app.listen(port, () => {
-//     console.log(`servidor corriendo en http://localhost:${port}`);
-// })
+const cart = require('./cart/buy.json'); 
 
-const cart = 'backend/emercado-api-main/cart/buy.json';
-getJSONData(cart).then(function(resultado) {
-    if (resultado.status === 'ok') {
-        console.log(resultado.data);
-    } else {
-        console.error("Error al obtener los datos:", resultado.status);
-    }
-}).catch(function(error) {
-    console.error("Error en la solicitud:", error);
-});
+app.get( '/cart' , (req, res) => {
+    res.json (cart);
+}); 
+
+//iniciar el servidor 
+app.listen(port, () => {
+    console.log(`servidor corriendo en http://localhost:${port}`);
+})
+
+
+
